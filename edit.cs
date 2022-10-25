80b9045ed49a581e9ef9ce88e252e7ba3c873b32
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-10-25 12:13:37 +0530

    edit using name

---
+++ edit.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace addressbookd9
+{
+    internal class edit
+    {
+        class address
+        {
+            public address(string name, string address)
+            {
+            }
+
+            public bool add(string name, string address)
+            {
+                address addr = new address(name, address);
+                address result = find(name);
+
+                if (result == null)
+                {
+                    object value = address.Add(addr);
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            private address find(string name)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
