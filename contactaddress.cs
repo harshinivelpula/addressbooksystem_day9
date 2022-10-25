a71acf2336fa348ec85a5d3d13d0564716aa9890
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-10-25 12:10:46 +0530

    giving input

---
+++ contactaddress.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace addressbookd9
+{
+    internal class contactaddress
+    {
+        internal static void address()
+        {
+            throw new NotImplementedException();
+        }
+
+        class Address
+        {
+            public string name;
+            public string address;
+            public Address(string name, string address)
+            {
+                this.name = name;
+                this.address = address;
+            }
+        }
+    }
+}
