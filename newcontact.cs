5860287ee375d0e45e17a5d93056cbdccd738df5
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-10-25 12:12:07 +0530

    adding new contact

---
+++ newcontact.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace addressbookd9
+{
+    internal class newcontact
+    { 
+        List<address> addresses;
+
+        public contact()
+        {
+            addresses = new List<address>();
+        }
+
+        internal static void Address()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
